33c3c5d30ce0a5a0dea6ea6d00d6aaa1d6b6692c
Carlotta Dimatteo <carlottaunspekable@gmail.com>
2024-09-11 20:24:36 +0200

    feat: calculate optimal k using elbow method and silhouette score

--- k-means.R
+++ k-means.R
@@ -74,6 +74,66 @@ head(dataset)
 summary(dataset)
 summary(features)
 head(features)
+
+# normalizzazione
 data_normalized <- scale(features[, c("Recency", "Frequency", "Monetary")]) 
 
 head(data_normalized)
+
+## applicazione del k-means
+
+## elbow method
+# 
+install.packages("factoextra")
+install.packages("ggplot2")
+
+library(factoextra)
+library(ggplot2)
+
+set.seed(123)  # Per rendere i risultati riproducibili
+
+# Esegui k-means per un range di cluster (ad esempio da 1 a 10 cluster)
+wcss <- function(k) {
+  kmeans(data_normalized, k, nstart = 25)$tot.withinss
+}
+
+# Calcola WCSS per ciascun valore di k
+k.values <- 1:10
+wcss_values <- sapply(k.values, wcss)
+
+# Crea un grafico per visualizzare il gomito
+plot(k.values, wcss_values, type="b", pch = 19, frame = FALSE,
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares (WCSS)",
+     main="Elbow Method for Optimal K")
+
+
+# Metodo del Silhouette Score 
+
+install.packages("cluster")
+
+library(cluster) 
+
+set.seed(123)  # Per rendere i risultati riproducibili
+
+# Funzione per calcolare il Silhouette Score per ogni numero di cluster
+calculate_silhouette <- function(k) {
+  km.res <- kmeans(data_normalized, centers = k, nstart = 25)
+  ss <- silhouette(km.res$cluster, dist(data_normalized))
+  mean(ss[, 3])  # Ritorna il punteggio medio silhouette
+}
+
+# Valori di k da testare (ad esempio, da 2 a 10 cluster)
+k.values <- 2:10
+silhouette_scores <- sapply(k.values, calculate_silhouette)
+
+
+# Crea un grafico del Silhouette Score per ciascun valore di k
+plot(k.values, silhouette_scores, type = "b", pch = 19, frame = FALSE,
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouette Score",
+     main = "Silhouette Method for Optimal K")
+
+
+# In entrambe le analisi il numero di k ottimale è 8
+
